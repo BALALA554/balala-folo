f0fe2ab4b0cde39f2f9bfabe849ca9d70d686e00
BALALA554 <soka43296@gmail.com>
2026-02-01 22:30:09 +0100

    Revise Firebase config and initialize services
    Updated Firebase configuration and initialized app, auth, and Firestore.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXX",
+  apiKey: "AIzaSyBXXXXXXXX",
   authDomain: "balala-folo.firebaseapp.com",
   projectId: "balala-folo",
   storageBucket: "balala-folo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  messagingSenderId: "XXXXXXXX",
+  appId: "1:XXXXXXXX:web:XXXXXXXX"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
